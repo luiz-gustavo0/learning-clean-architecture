0498c281d970a4ef171b35b53da75cfef864f5e2
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-07-22 15:19:00 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,6 @@
 
 module.exports = {
 
-  collectCoverage: true,
-
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   coverageDirectory: 'coverage',
---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  it('should return 400 if no name is provid', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      bdoy: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
