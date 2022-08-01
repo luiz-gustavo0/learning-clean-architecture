ba7160642df075216a171b252ba24edff2708954
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-08-01 14:25:01 -0300

    feat: ensure DbAuthentication call UpadateAccesTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -7,28 +7,33 @@ import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import {
   LoadAccountByEmailRepository
 } from '../../protocols/db/load-account-by-email-repository'
+import { UpadateAccesTokenRepository } from '../../protocols/db/update-accessToken-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly upadateAccesTokenRepository: UpadateAccesTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator
+    tokenGenerator: TokenGenerator,
+    upadateAccesTokenRepository: UpadateAccesTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.upadateAccesTokenRepository = upadateAccesTokenRepository
   }
 
-  async auth (authentication: AuthenticationModel): Promise<string> {
-    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+  async auth ({ email, password }: AuthenticationModel): Promise<string> {
+    const account = await this.loadAccountByEmailRepository.load(email)
     if (account) {
-      const isValid = await this.hashComparer.comparer(authentication.password, account.password)
+      const isValid = await this.hashComparer.comparer(password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.upadateAccesTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
